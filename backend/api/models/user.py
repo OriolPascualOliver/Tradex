5527697e22cedf44c99e40d5e4b5e848bab0273b
Oriol Pascual <69343366+OriolPascualOliver@users.noreply.github.com>
2025-09-02 18:27:30 +0200

    Track first sign-in date

--- backend/api/models/user.py
+++ backend/api/models/user.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, DateTime
 from sqlalchemy.orm import relationship
 
 from core.database import Base
@@ -12,6 +12,23 @@ class User(Base):
     hashed_password = Column(String, nullable=False)
     is_active = Column(Boolean, default=True)
     device_id = Column(String, nullable=True)
+
+    # Registration fields
+    license = Column(String, nullable=True)
+    team_members = Column(Integer, nullable=True)
+    telephone = Column(String, nullable=True)
+    first_name = Column(String, nullable=True)
+    surname1 = Column(String, nullable=True)
+    surname2 = Column(String, nullable=True)
+    nif = Column(String, nullable=True)
+    company_name = Column(String, nullable=True)
+    sector = Column(String, nullable=True)
+    country = Column(String, nullable=True)
+    state = Column(String, nullable=True)
+    zip_code = Column(String, nullable=True)
+    terms_accepted = Column(Boolean, default=False, nullable=False)
+    last_login = Column(DateTime, nullable=True)
+    sign_in_date = Column(DateTime, nullable=True)
 
     tasks = relationship("Task", back_populates="owner")
 
