e44661ba4bfe4ce246d39d99b63ec7800c7e984d
Oriol Pascual <69343366+OriolPascualOliver@users.noreply.github.com>
2025-09-15 10:56:17 +0200

    chore: update email env secrets

--- backend/api/schemas/task.py
+++ backend/api/schemas/task.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 from typing import Optional
 
 
@@ -20,5 +20,4 @@ class TaskRead(TaskBase):
     id: int
     owner_id: int
 
-    class Config:
-        orm_mode = True
+    model_config = ConfigDict(from_attributes=True)
